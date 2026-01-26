d2948e059cdedd32d0f5b88b11221328a143bdd4
Rubakkumar S <169072988+rubak1@users.noreply.github.com>
2026-01-26 22:32:08 +0530

    Add files via upload

---
+++ boolean.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int nithishGay = true ;
+    int nithishlesibisan = false;
+    
+    printf("%d",nithishGay);
+    
+    return 0;
+}
