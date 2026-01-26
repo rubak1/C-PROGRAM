7fb4d611fb31a11fe7db953539f40d2cc3f40047
Rubakkumar S <169072988+rubak1@users.noreply.github.com>
2026-01-26 22:31:39 +0530

    Add files via upload

---
+++ arithmatic operation.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int x=5,y=6;
+int x1=x+y;
+printf("%d\n",x1);
+int x2=x-y;
+printf("%d\n",x2);
+int x3=x*y;
+printf("%d\n\n\n",x3);
+
+printf("increment and decrement\n\n");
+int z=5;
+
+printf("%d\n",z++);
+printf("%d\n",z--);
+
+return 0;
+}
