da15d1521fd994fc8272c885308233b974969500
Rubakkumar S <169072988+rubak1@users.noreply.github.com>
2026-01-27 20:49:54 +0530

    Add files via upload

---
+++ nested if.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the number: ");
+    scanf("%d",&n);
+    
+    int f;
+    printf("enter the number 2: \n\n");
+    scanf("%d",&f);
+    
+    if (n<20) {
+        printf("nithish love dharshini\n");
+        if (f<20) {
+            printf("nitish love gopika ");
+        }
+    }
+    return 0;
+}
