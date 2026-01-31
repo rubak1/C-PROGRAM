9d4b4e282aca7b52d6c5c167101a66e05f3b6cab
Rubakkumar S <169072988+rubak1@users.noreply.github.com>
2026-01-31 23:46:10 +0530

    Add files via upload

---
+++ character more than by 2 string hack .c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+#include <string.h>
+int main() {
+    char s[20000];
+    scanf("%s",s);
+  
+  int n=strlen(s);
+    
+  int freq[256]={0};
+    for(int i=0;i<n;i++){
+        freq[s[i]]++;
+    }
+        
+        for(int i=0;i<n;i++){
+            if(freq[s[i]] > n/2){
+                printf("%c",s[i]);
+                return 0;
+            }
+        }
+
+        printf("-1");
+        return 0;
+}
